1160518f85065a5f00d597f4d8788956ca099b6b
juvo <juvor@outlook.com>
2026-01-19 16:21:58 +0100

    fix: fixed code in preprocessing

--- 1_PREPROCESSING/preparation.py
+++ 1_PREPROCESSING/preparation.py
@@ -7,24 +7,15 @@ IN_PREPROCESS = "./1_PREPROCESSING/IN_PREPROCESS/"
 OUT_PREPROCESS = "./2_PROCESSING/CLASSIC_CV/OUT_PREPROCESS/"
 TRAINING_DATA = "./2_PREPROCESSING/UNET/TRAINING_ARRAY/"
 
-PREPROCESS_DATA = sorted(os.listdir(IN_PREPROCESS))
-
-groups = itertools.groupby(PREPROCESS_DATA, key=lambda x: x.split(".")[0][:-1])
-
-# Use when naming convention is new with _
-# PREPROCESSED_IMG = sorted([f for f in os.listdir(PREPROCESSED_DIR) if f.endswith('.png')])
-# groups = itertools.groupby(PREPROCESSED_IMG, key=lambda x: x.split("_")[0])
+PREPROCESSED_IMG = sorted([f for f in os.listdir(IN_PREPROCESS) if f.endswith('.png')])
+groups = itertools.groupby(PREPROCESSED_IMG, key=lambda x: x.split("_")[0])
 
 for sample, images in groups:
-
     A_B_G_R_list = sorted(list(images))
     colour_paths = [os.path.join(IN_PREPROCESS, x) for x in A_B_G_R_list]
 
     processor = Preprocessor(
-        colour_paths[0], 
-        colour_paths[1], 
-        colour_paths[2], 
-        colour_paths[3]
+        colour_paths[0], colour_paths[1], colour_paths[2], colour_paths[3]
     )
 
     processor.process_clahe_intensity()
